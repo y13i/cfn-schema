50064a86b36f99644e5964a37346029fe0e78f35
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2019-10-02 22:44:51 +0100

    Update rebuildPush.sh

--- scripts/rebuildPush.sh
+++ scripts/rebuildPush.sh
@@ -6,8 +6,10 @@ git config user.name ${GITHUB_ACTOR}
 git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
 
 TIMESTAMP=`date +%s | tr -d '\n'`
+BRANCH="rebuild/${TIMESTAMP}"
 
 git checkout -b rebuild/${TIMESTAMP}
+git remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 npm install
 npm run build
@@ -17,7 +19,7 @@ if git diff-index --quiet HEAD --; then
 else
   git add .
   git commit -m "rebuild"
-  git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  git push origin ${BRANCH}
 fi
 
 exit $?
